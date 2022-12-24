b242df7d9f12977aee0acd8a207aa6a7fa01b671
Kuldeep147 <kuldeep.bargurjar@gmail.com>
2022-12-24 16:51:11 +0530

    gambling uc1 added

--- src/com/bzl/gambling/Gambling.java
+++ src/com/bzl/gambling/Gambling.java
@@ -1,6 +1,14 @@
 package com.bzl.gambling;
+
+import java.util.Random;
+
 public class Gambling {
     public static void main(String[] args) {
         System.out.println("Hello Gambling simulation");
     }
+    int stake = 100;
+    int bet = 1;
+    int win = 0;
+    int loose = 0;
+    
 }
