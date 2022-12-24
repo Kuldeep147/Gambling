1365312cc8f2f72c1c0e5edcd3bd28cf1cd58c47
Kuldeep147 <kuldeep.bargurjar@gmail.com>
2022-12-24 17:50:31 +0530

    added gambling uc3

--- src/com/bzl/gambling/Gambling.java
+++ src/com/bzl/gambling/Gambling.java
@@ -5,22 +5,36 @@ import java.util.Random;
 public class Gambling {
     public static void main(String[] args) {
         System.out.println("Hello Gambling simulation");
+        Gambling ob = new Gambling(100);
+        while (ob.resign == false) {
+            ob.betStake(1);
+        }
     }
-    int stake = 100;
-    int bet = 1;
+    int stake ;
     int win = 0;
+    int tempStake;
     int loose = 0;
-    public void bet(int p){
+    boolean resign = false ;
+    public Gambling(int stake){
+        this.stake = stake;
+        tempStake = stake;
+    }
+    public void betStake(int p){
         Random r = new Random();
         int i = r.nextInt(2);
-        if(i == 0){
-            stake = stake - p;
-            System.out.println("loose");
-            loose += p;
+        if(stake <= tempStake * 1.5 && stake >= tempStake * 0.5) {
+            if (i == 0) {
+                stake = stake - p;
+                System.out.println("loose");
+                loose += p;
+            } else {
+                System.out.println("win");
+                win += p;
+                stake = stake + p;
+            }
         }else{
-            System.out.println("win");
-            win += p;
-            stake = stake + p;
+            resign = true;
+            System.out.println("day "+stake);
         }
     }
 }
