67d5d1d688bb1c658992fd0c9fe77a790c8b20f5
Kuldeep147 <kuldeep.bargurjar@gmail.com>
2022-12-24 17:10:02 +0530

    adding uc2 gambling

--- src/com/bzl/gambling/Gambling.java
+++ src/com/bzl/gambling/Gambling.java
@@ -10,5 +10,17 @@ public class Gambling {
     int bet = 1;
     int win = 0;
     int loose = 0;
-    
+    public void bet(int p){
+        Random r = new Random();
+        int i = r.nextInt(2);
+        if(i == 0){
+            stake = stake - p;
+            System.out.println("loose");
+            loose += p;
+        }else{
+            System.out.println("win");
+            win += p;
+            stake = stake + p;
+        }
+    }
 }
